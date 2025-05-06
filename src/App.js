64f442ffe0124061f8159e00c5aca809d55d6faa
Kartik23411 <Kartik110605@gmail.com>
2025-05-06 16:11:33 +0530

    Added the routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import UploadPage from './components/UploadPage';
+import FilesPage from './components/FilesPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Router>
+     <nav>
+        <Link to= "/">Upload</Link> | <Link to="/files">Files</Link>
+     </nav>
+     <Routes>
+        <Route path="/" element={<UploadPage/> }/>
+        <Route path="/files" element={<FilesPage/> }/>
+     </Routes>
+   </Router>
   );
 }
 
